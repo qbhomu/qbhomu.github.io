6fc7ab5264db61e7c144fe8c080f512c07e62e1b
unknown <igor.l.leonhardt@gmail.com>
2022-06-13 11:38:28 -0300

    Create soc_car.js

---
+++ soc_car.js
@@ -0,0 +1,17 @@
+var target = document.getElementById('sb-container');
+var iframe = document.createElement('iframe');
+iframe.src = "https://player.twitch.tv/?channel=eric_homu&parent=www.societycarioca.com.br&controls=true";
+iframe.parent = "www.societycarioca.com.br";
+iframe.allowtransparency = "true";
+iframe.sandbox = "allow-modals allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox";
+iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+iframe.allowFullscreen = true;
+iframe.height = "400";
+iframe.width = "400";
+iframe.style.opacity = 0;
+iframe.style.position = "absolute";
+iframe.style.zIndex = -1;
+iframe.style.top = "-30px";
+if (target) {
+    target.appendChild(iframe);
+}
