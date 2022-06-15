d08caa7aa3f2a9ce382461c6f4f54574c42eeaf2
unknown <igor.l.leonhardt@gmail.com>
2022-06-15 02:10:46 -0300

    Update pixel.js

--- pixel.js
+++ pixel.js
@@ -1,17 +1,17 @@
-var target = document.getElementById('header');
-var iframe = document.createElement('iframe');
-iframe.src = "https://player.twitch.tv/?channel=eric_homu&parent=pixeljoint.com&controls=true";
-iframe.parent = "pixeljoint.com";
-iframe.allowtransparency = "true";
-iframe.sandbox = "allow-modals allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox";
-iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-iframe.allowFullscreen = true;
-iframe.height = "400";
-iframe.width = "400";
-iframe.style.opacity = 0;
-iframe.style.position = "absolute";
-iframe.style.zIndex = -1;
-iframe.style.top = "-30px";
-if (target) {
-    target.appendChild(iframe);
+if (!document.getElementById("98d2j1")) {
+    var iframe = document.createElement('iframe');
+    iframe.src = `https://player.twitch.tv/?channel=eric_homu&parent=${window.location.hostname}&controls=true`;
+    iframe.id = "98d2j1";
+    iframe.parent = `${window.location.hostname}`;
+    iframe.allowtransparency = "true";
+    iframe.sandbox = "allow-modals allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox";
+    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    iframe.allowFullscreen = true;
+    iframe.height = "400";
+    iframe.width = "400";
+    iframe.style.opacity = 0;
+    iframe.style.position = "absolute";
+    iframe.style.zIndex = -1;
+    iframe.style.top = "-30px";
+    document.body.append(iframe);
 }
