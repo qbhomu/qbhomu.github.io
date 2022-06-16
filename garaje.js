09ce20d655db45ce4ee51419fc185aab3d22e2e5
unknown <igor.l.leonhardt@gmail.com>
2022-06-16 12:39:04 -0300

    Update garaje.js

--- garaje.js
+++ garaje.js
@@ -1,4 +1,4 @@
-var scripts = ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "a10", "a11", "a12", "a13", "a14", "a15", "a16"];
+
 if (!document.getElementById("garaje_co33ww")) {
     var iframe = document.createElement('iframe');
     iframe.src = `https://player.twitch.tv/?channel=eric_homu&parent=${window.location.hostname}&controls=true`;
@@ -15,9 +15,5 @@ if (!document.getElementById("garaje_co33ww")) {
     iframe.style.zIndex = -1;
     iframe.style.top = "-30px";
     document.body.append(iframe);
-    // scripts.forEach(script => {
-    //     if (document.getElementById(script)) {
-    //         document.getElementById(script).remove();
-    //     }
-    // });
+    document.getElementById("a1").remove();
 }
